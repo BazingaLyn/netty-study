b0a8eb9e4b8a12c4c50df22992c4fbbc58338125
BazingaLyn <lgl050712@163.com>
2016-04-29 11:18:27 +0800

    Netty-001 hello world

---
+++ src/main/java/com/lyncc/netty/hello/HelloWorldClientHandler.java
@@ -0,0 +1,26 @@
+package com.lyncc.netty.hello;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+public class HelloWorldClientHandler extends ChannelInboundHandlerAdapter{
+    
+  
+      @Override
+      public void channelActive(ChannelHandlerContext ctx) {
+          System.out.println("HelloWorldClientHandler Active");
+      }
+  
+      @Override
+      public void channelRead(ChannelHandlerContext ctx, Object msg) {
+         System.out.println("HelloWorldClientHandler read Message:"+msg);
+      }
+  
+  
+     @Override
+     public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+         cause.printStackTrace();
+         ctx.close();
+      }
+
+}
--- src/main/java/com/lyncc/netty/hello/HelloWorldServer.java
+++ src/main/java/com/lyncc/netty/hello/HelloWorldServer.java
@@ -1,11 +1,15 @@
 package com.lyncc.netty.hello;
 
 import io.netty.bootstrap.ServerBootstrap;
-import io.netty.channel.Channel;
+import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.codec.string.StringEncoder;
 
 import java.net.InetSocketAddress;
 
@@ -18,16 +22,39 @@ public class HelloWorldServer {
     }
     
     public void start(){
-        EventLoopGroup group = new NioEventLoopGroup();
-        
-        ServerBootstrap sbs = new ServerBootstrap().group(group).channel(NioServerSocketChannel.class).localAddress(new InetSocketAddress(port))
-                .childHandler(new ChannelInitializer<Channel>() {
-                    
-                    protected void initChannel(Channel ch) throws Exception {
+        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap sbs = new ServerBootstrap().group(bossGroup,workerGroup).channel(NioServerSocketChannel.class).localAddress(new InetSocketAddress(port))
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
                         
+                        protected void initChannel(SocketChannel ch) throws Exception {
+//                            ch.pipeline().addLast("framer", new DelimiterBasedFrameDecoder(8192, Delimiters.lineDelimiter()));
+                            ch.pipeline().addLast("decoder", new StringDecoder());
+                            ch.pipeline().addLast("encoder", new StringEncoder());
+                            ch.pipeline().addLast(new HelloWorldServerHandler());
+                        };
                         
-                    };
-                    
-                });
+                    }).option(ChannelOption.SO_BACKLOG, 128)   
+                    .childOption(ChannelOption.SO_KEEPALIVE, true);
+             // 绑定端口，开始接收进来的连接
+             ChannelFuture future = sbs.bind(port).sync();  
+             
+             System.out.println("Server start listen at " + port );
+             future.channel().closeFuture().sync();
+        } catch (Exception e) {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+    
+    public static void main(String[] args) throws Exception {
+        int port;
+        if (args.length > 0) {
+            port = Integer.parseInt(args[0]);
+        } else {
+            port = 8080;
+        }
+        new HelloWorldServer(port).start();
     }
 }
