605162e80f529796c03aa8bb99d3dc70079d4ab1
BazingaLyncc <lgl050712@163.com>
2016-05-02 17:42:20 +0800

    Netty-003 conpect

--- src/main/java/com/lyncc/netty/concept/HelloWorldConcept.java
+++ src/main/java/com/lyncc/netty/concept/HelloWorldConcept.java
@@ -15,20 +15,26 @@ public class HelloWorldConcept {
      * 
      *                   ________________________                                 __________________________
      *                  |                        |                               |                          |    
-     *                  |   <-----Inbound-----   |                               |                          |
-     *                  |   _____        ______  |                               |                          |
-     *      _______     |  |     |       |    |  |                               |                          |
-     *     |       |    |  |     |       |    |  |      ___________________      |                          |
-     *     |       |    |  |_____|       |____|  |     |                   |     |                          |  
-     *     |client |----|-------______-----------|-----|      network      |-----|--------------------------|--
-     *     |       |    |       |     |          |     |___________________|     |                          |
-     *     |       |    |       |     |          |                               |                          | 
-     *     |       |    |       |_____|          |                               |                          |
-     *     |       |    |                        |                               |                          |
-     *     |_______|    |   -----Ountbound--->   |                               |                          | 
-     *                  |___ChannelPipeline______|                               |__________________________| 
-     * 
-     * 
+     *                  |   <-----Inbound-----   |                               |   ---inbound------- >    |   ________
+     *                  |   _____        ______  |                               |    _______      ____     |  |        |
+     *      _______     |  |     |       |    |  |                               |    |     |     |    |    |  |        |  
+     *     |       |    |  |  ②  |       |  ③ |  |      ___________________      |    |  ⑤  |     |  ⑥ |    |  |        |
+     *     |       |    |  |_____|       |____|  |     |                   |     |    |_____|     |____|    |  |        |     
+     *     |client |----|-------______-----------|-----|      network      |-----|--------------------------|--| server |
+     *     |       |    |       |     |          |     |___________________|     |          ______          |  |        |
+     *     |       |    |       |  ①  |          |                               |          |     |         |  |        |         
+     *     |       |    |       |_____|          |                               |          |  ④  |         |  |________|
+     *     |       |    |                        |                               |          |_____|         |
+     *     |_______|    |   -----Outbound--->    |                               |    <-----outbound----    | 
+     *                  |___ChannelPipeline______|                               |______ChannelPipeline_____| 
+     *                                                                               
+     *  ①：StringEncoder继承于MessageToMessageEncoder，而MessageToMessageEncoder又继承于ChannelOutboundHandlerAdapter
+     *  ②：HelloWorldClientHandler.java
+     *  ③：StringDecoder继承于MessageToMessageDecoder，而MessageToMessageDecoder又继承于ChannelInboundHandlerAdapter
+     *  ④：StringEncoder 编码器
+     *  ⑤：StringDecoder 解码器
+     *  ⑥：HelloWorldServerHandler.java
+     *  
      * 
      * 
      * 
